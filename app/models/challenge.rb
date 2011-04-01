e4bb9ae35225f93aec1dff9c53911714929fc28a
Taryn East <git@taryneast.org>
2011-04-01 13:22:49 +0100

    Basic validations on challenges
    presence of name, numericality of estimate and completedin, start before
    stop.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,23 @@
 class Challenge < ActiveRecord::Base
+
+  ############
+  # validations
+  validates_presence_of :name
+
+  validates_numericality_of :estimate, :greater_than_or_equal_to => 0, :allow_nil => true, :only_integer => true
+  validates_numericality_of :completedin, :greater_than_or_equal_to => 0, :allow_nil => true, :only_integer => true
+
+  validate :start_before_stop
+
+
+  ############
+  # validation helpers
+  
+  def start_before_stop
+    return if started_at.blank? || stopped_at.blank?
+    errors.add :stopped_at, "must be after started-at!" if started_at >= stopped_at
+  end
+
+
 end
+# non-negative durations
--- test/unit/challenge_test.rb
+++ test/unit/challenge_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class ChallengeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "validations" do
+    should_validate_presence_of :name
+    should_validate_numericality_of :estimate
+    should_validate_numericality_of :completedin
+
+    should "require stop time after start time" do
+      start = 1.minute.ago
+      stop = 10.minutes.ago
+      assert (stop < start), "sanity check: we're testing that stop is before start"
+      c = Challenge.new(:name => 'new', :started_at => start, :stopped_at => stop)
+      assert !c.valid?, "our c should *not* be valid!!!" 
+      assert c.errors[:stopped_at].present?, "should have the error on stop-time, but had it on: #{c.errors.full_messages.inspect}"
+    end
   end
 end
