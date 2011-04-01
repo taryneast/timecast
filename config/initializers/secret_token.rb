a00188983d826269c7b0d5c3fcbe15a444a7fc66
Taryn East <git@taryneast.org>
2011-04-01 11:50:39 +0100

    First commit - empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timecast::Application.config.secret_token = '1982fff84b7cd013167195f06f653dc1a48bbda53ed97a251a63992da45f5ec8de0a75be1a1c8dd10d5b10f87234b9960002946d1bbdd070f286f0caa4eda1a3'
